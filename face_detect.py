793293641b9c33bdd0ba7525e99c8c8ec923bdd7
wtrmln <alexis.gelin@outlook.fr>
2020-11-12 15:27:38 +0100

    Exo 2 et 3 et README mis à jour

--- face_detect.py
+++ face_detect.py
@@ -1,25 +1,45 @@
 #!/usr/bin/python
 
+from urllib.parse import urlparse
+import numpy as np
+import urllib.request
 import cv2
 import sys
 
-if len(sys.argv) != 2:
-    print("Syntax error\nSyntax:", sys.argv[0], "[IMAGE PATH]")
-    exit(1)
+def url_to_img(url):
+    resp = urllib.request.urlopen(url)
+    img = np.asarray(bytearray(resp.read()), dtype="uint8")
+    return cv2.imdecode(img, cv2.IMREAD_COLOR)
 
-# On charge la cascade (pré-traitement pour reconnaitre des visages)
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+def detect_faces(img_path):
+    # On charge la cascade (pré-traitement pour reconnaitre des visages)
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-img = cv2.imread(sys.argv[1])  # On charge l'image
+    parsed_uri = urlparse(img_path)
+    scheme = '{uri.scheme}'.format(uri=parsed_uri)
+    if scheme == "http" or scheme == "https":
+        img = url_to_img(img_path)
+    else:
+        img = cv2.imread(img_path)  # On charge l'image
 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # Conversion niveaux de gris
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # Conversion niveaux de gris
 
-# On détecte les visages avec la cascade
-faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    # On détecte les visages avec la cascade
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 
-# On dessine les rectanges autour des visages
-for (x, y, w, h) in faces:
-    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    return faces, img
 
-cv2.imshow('img', img)  # On affiche la sortie
-cv2.waitKey()  # On attend une touche pour fermer
+def show_faces(faces, img):
+    # On dessine les rectanges autour des visages
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+    cv2.imshow('img', img)  # On affiche la sortie
+    cv2.waitKey()  # On attend une touche pour fermer
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Syntax error\nSyntax:", sys.argv[0], "[IMAGE PATH]")
+        exit(1)
+    faces, img = detect_faces(sys.argv[1])
+    show_faces(faces, img)
--- readme.md
+++ readme.md
@@ -1,1 +1,11 @@
-##Projet IA dans le cloud
+## Projet IA dans le cloud
+### Alexis GELIN--ANDRIEU
+Le premier exercice se trouve dans face_detect.py  
+`python face_detect.py [IMAGE URL]`  
+
+Le deuxième exercice se trouve dans app.py  
+`python app.py`  
+
+Le troisième exercice dans ia_cloud.ipynb  
+
+*Ne pas oublier de `source bin/activate`*
