b0ee5d508cf2d3b8610815bf55e474a314f997a3
ferrand-felix <ferrand.felix@gmail.com>
2020-11-19 14:05:24 +0100

    hello_world_id

--- felix_ferrand/django/ia_cloud/app_project/urls.py
+++ felix_ferrand/django/ia_cloud/app_project/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from app_project import views
 
 urlpatterns = [
-    path('project/hello_world', views.hello_world, name='hello_world')
+    path('project/hello_world', views.hello_world, name='hello_world'),
+    path('project/hello_world/<id>', views.hello_world_id, name='hello_world_id'),
 ]
--- felix_ferrand/django/ia_cloud/app_project/views.py
+++ felix_ferrand/django/ia_cloud/app_project/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def hello_world(request):
     return render(request, 'app_project/hello_world.html', locals())
+
+def hello_world_id(request, id):
+    return render(request, 'app_project/hello_world_id.html', locals())
