c5ef7dae085e9ff1b4639885cc41d02af4128b32
ferrand-felix <ferrand.felix@gmail.com>
2020-11-19 15:28:14 +0100

    add signup

--- felix_ferrand/django/ia_cloud/app_project/models.py
+++ felix_ferrand/django/ia_cloud/app_project/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE),
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
     firstname = models.CharField(max_length=200, null=True)
     lastname = models.CharField(max_length=200, null=True)
     location = models.CharField(max_length=200, null=True)
--- felix_ferrand/django/ia_cloud/app_project/views.py
+++ felix_ferrand/django/ia_cloud/app_project/views.py
@@ -1,9 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from app_project.models import Profile
+from app_project.forms import SignUpForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-def hello_world(request):
-    return render(request, 'app_project/hello_world.html', locals())
+def profile(request):
+    return render(request, 'app_project/profile.html', locals())
 
-def hello_world_id(request, id):
-    return render(request, 'app_project/hello_world_id.html', locals())
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get('email')
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = User.objects.create_user(username, email, password)
+            user.is_active = True
+            user.save()
+            profile = Profile(user=user)
+            profile.save()
+            login(request, user, backend='django.contrib.auth.backends.MoveBackend')
+            return redirect('profile')
+        else:
+            return render(request, 'app_project/signup.html', {
+                'form': form,
+            })
+    else:
+        form = SignUpForm()
+        return render(request, 'app_project/signup.html', {
+            'form': form,
+        })
+
+# def signin(request):
