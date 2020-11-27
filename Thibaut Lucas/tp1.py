0ba69fc760ba8929feda88f21c713a102787aaf2
ThibautLucas <thibautlucas13@gmail.com>
2020-11-27 13:22:57 +0100

    v2

--- Thibaut Lucas/tp1.py
+++ Thibaut Lucas/tp1.py
@@ -185,3 +185,26 @@ while not done:
     
 
 ### Display the time steps and the penalties 
+
+
+
+
+""" 
+Ok now let's visualize it 
+
+"""
+
+from IPython.display import clear_output
+from time import sleep
+
+def print_frames(frames):
+    for i, frame in enumerate(frames):
+        clear_output(wait=True)
+        print(frame['frame'].getvalue())
+        print(f"Timestep: {i + 1}")
+        print(f"State: {frame['state']}")
+        print(f"Action: {frame['action']}")
+        print(f"Reward: {frame['reward']}")
+        sleep(.1)
+        
+print_frames(frames)
